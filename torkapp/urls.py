29032fdfb9a417ffeda8927eae2cef371617d7d1
Victor Lara <lara.m.victor@gmail.com>
2014-07-30 00:15:09 -0700

    added uploadcsv view

--- torkapp/urls.py
+++ torkapp/urls.py
@@ -3,5 +3,6 @@ from django.conf.urls import url
 from torkapp import views
 
 urlpatterns = [
-	url(r'^$', views.index, name='index')
+	url(r'^$', views.index, name='index'),
+	url(r'^csvupload/', views.csvupload, name="csvupload"),
 ]
